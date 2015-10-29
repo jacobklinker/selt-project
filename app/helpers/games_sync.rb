89ab0f27d248a82f765179baba104e7570c38b7e
jacobklinker <jacob-klinker@uiowa.edu>
2015-10-29 15:06:23 +0000

    add testing for background sync

--- app/helpers/games_sync.rb
+++ app/helpers/games_sync.rb
@@ -3,8 +3,10 @@ require 'open-uri'
 class GamesSync
   
   def perform
-    doc = Nokogiri::HTML(open("http://xml.pinnaclesports.com/pinnaclefeed.aspx?sporttype=Football&sportsubtype=ncaa"))
+    doc = Nokogiri::XML(GamesSync.get_xml())
     feed_time = doc.xpath('/pinnacle_line_feed/PinnacleFeedTime')
+    puts feed_time.text
+    
     sync = Sync.new
     
     sync.pinnacle_feed_time = feed_time
@@ -15,8 +17,14 @@ class GamesSync
     sync.is_successful = true
     
     # TODO perform nokogiri processing here
+    nodes = doc.xpath('//event_datetimeGMT or //participant_name or //spread_visiting or //spread_home')
+    puts nodes
     
     sync.save
+  end
+  
+  def self.get_xml
+    open("http://xml.pinnaclesports.com/pinnaclefeed.aspx?sporttype=Football&sportsubtype=ncaa")
   end
   
 end
