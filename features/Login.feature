0a6e43e0221bf0716aab1771288bb391c15c0bac
Luke Klinker <lucas-klinker@uiowa.edu>
2015-10-30 19:30:21 +0000

    added happy and sad path for logging in with users

--- features/Login.feature
+++ features/Login.feature
@@ -1,4 +1,4 @@
-Feature: Allow a user to login to the app
+Feature: Users can create an account and login
 
 Scenario: Reach the login page
   When I am on the homepage
@@ -8,5 +8,18 @@ Scenario: Reach the login page
   And I should see "Remember me"
   
 Scenario: Create new user
-  When I have added a user with email "test@test.com" and password "password"
+  When I signed up a user with email "test@test.com" and password "password"
   Then I should see "A message with a confirmation link has been sent to your email address. Please follow the link to activate your account."
+  
+Scenario: Login to unconfirmed account
+  When I signed up a user with email "test@test.com" and password "password"
+  And I login with "test@test.com" and password "password"
+  Then I should see "You have to confirm your email address before continuing."
+  
+Scenario: Login to confirmed account
+  Given the following users have been added:
+  | email          | password |
+  | test@test.com  | password |
+  
+  When I login with "test@test.com" and password "password"
+  Then I should see "Signed in successfully."
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,9 +2,13 @@
   visit root_path
  end
  
- Given /^I am on the create user screen$/ do
-  visit users_sign_up_path
- end
+ Given /the following users have been added:/ do |users_table|
+  users_table.hashes.each do |user|
+   user = User.new(:email => user[:email], :password => user[:password], :password_confirmation => user[:password])
+   user.skip_confirmation!
+   user.save!
+  end
+end
  
  When /^I click the "(.*?)" link$/ do |id|
   click_on id
@@ -14,11 +18,7 @@
   click_button id
  end
  
- Then /^I should see "(.*?)"$/ do |text|
-  expect(page).to have_content(text)
- end
- 
- When /^I have added a user with email "(.*?)" and password "(.*?)"$/ do |email, password|
+ When /^I signed up a user with email "(.*?)" and password "(.*?)"$/ do |email, password|
   visit users_sign_up_path
   
   fill_in "user_email", :with => email
@@ -27,5 +27,17 @@
   
   click_button 'create'
  end
+ 
+ When /^I login with "(.*?)" and password "(.*?)"$/ do |email, password|
+  visit users_sign_in_path
   
+  fill_in "user_email", :with => email
+  fill_in "user_password", :with => password
+  
+  click_button 'log_in'
+ end
+ 
+ Then /^I should see "(.*?)"$/ do |text|
+  expect(page).to have_content(text)
+ end
   
