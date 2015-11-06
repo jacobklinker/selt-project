40918dcf92db5e93ce4001b7443a45f982112b76
Luke Klinker <lucas-klinker@uiowa.edu>
2015-11-06 03:10:48 +0000

    fix the prod env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  before_filter :auth_outside_test
-  
-  protected
-    # this method should be placed in ApplicationController
-    def auth_outside_test
-      head(:authenticate_user!) unless Rails.env.test?
-    end
+  before_filter :authenticate_user! unless Rails.env.test?
 end
