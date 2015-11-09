5ba33cf0b5d1b8723e5c862165574631509b13ce
tysonmassey <tyson-massey@uiowa.edu>
2015-11-09 01:45:13 +0000

    trying to fix database issue with is_successful

--- db/migrate/20151030145159_add_defaults_to_score_sync.rb
+++ db/migrate/20151030145159_add_defaults_to_score_sync.rb
@@ -2,6 +2,6 @@ class AddDefaultsToScoreSync < ActiveRecord::Migration
   def change
     change_column :score_syncs, :tweets_found, :integer, :default => 0
     change_column :score_syncs, :tweets_used, :integer, :default => 0
-    change_column :score_syncs, :is_successful, :boolean, :default => true
+    change_column :score_syncs, :is_successful, 'boolean USING CAST(is_successful AS boolean)', :default => true
   end
 end
