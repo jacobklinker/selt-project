be6fddd4f320f5ff8284d30bbd0049343401048f
tysonmassey <tyson-massey@uiowa.edu>
2015-11-09 02:09:05 +0000

    gave up fixing is_successful boolean issue

--- db/migrate/20151029151640_add_defaults_to_games_and_syncs.rb
+++ db/migrate/20151029151640_add_defaults_to_games_and_syncs.rb
@@ -6,6 +6,6 @@ class AddDefaultsToGamesAndSyncs < ActiveRecord::Migration
     change_column :syncs, :new_games, :integer, :default => 0
     change_column :syncs, :updated_games, :integer, :default => 0
     change_column :syncs, :failed_games, :integer, :default => 0
-    change_column :syncs, :is_successful, 'boolean USING CAST(is_successful AS boolean)', :default => true
+    change_column :syncs, :is_successful, :boolean, :default => true
   end
 end
