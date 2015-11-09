5d590cbffa789b7e09efa0c128526e1c0beec72b
tysonmassey <tyson-massey@uiowa.edu>
2015-11-09 21:06:43 +0000

    deleted fk on commish

--- db/migrate/20151104210354_create_leagues.rb
+++ db/migrate/20151104210354_create_leagues.rb
@@ -3,30 +3,30 @@ class CreateLeagues < ActiveRecord::Migration
     create_table :leagues do |t|
       t.string :league_name
       t.references :commissioner, index: true
-      t.references :current_leader, index: true, foreign_key: true
+      t.references :current_leader, index: true
       t.string :conference_settings
       t.integer :number_picks_settings
       t.integer :number_members
-      t.references :user1, index: true, foreign_key: true
-      t.references :user2, index: true, foreign_key: true
-      t.references :user3, index: true, foreign_key: true
-      t.references :user4, index: true, foreign_key: true
-      t.references :user5, index: true, foreign_key: true
-      t.references :user6, index: true, foreign_key: true
-      t.references :user7, index: true, foreign_key: true
-      t.references :user8, index: true, foreign_key: true
-      t.references :user9, index: true, foreign_key: true
-      t.references :user10, index: true, foreign_key: true
-      t.references :user11, index: true, foreign_key: true
-      t.references :user12, index: true, foreign_key: true
-      t.references :user13, index: true, foreign_key: true
-      t.references :user14, index: true, foreign_key: true
-      t.references :user15, index: true, foreign_key: true
-      t.references :user16, index: true, foreign_key: true
-      t.references :user17, index: true, foreign_key: true
-      t.references :user18, index: true, foreign_key: true
-      t.references :user19, index: true, foreign_key: true
-      t.references :user20, index: true, foreign_key: true
+      t.references :user1, index: true
+      t.references :user2, index: true
+      t.references :user3, index: true
+      t.references :user4, index: true
+      t.references :user5, index: true
+      t.references :user6, index: true
+      t.references :user7, index: true
+      t.references :user8, index: true
+      t.references :user9, index: true
+      t.references :user10, index: true
+      t.references :user11, index: true
+      t.references :user12, index: true
+      t.references :user13, index: true
+      t.references :user14, index: true
+      t.references :user15, index: true
+      t.references :user16, index: true
+      t.references :user17, index: true
+      t.references :user18, index: true
+      t.references :user19, index: true
+      t.references :user20, index: true
       t.timestamps null: false
     end
   end
