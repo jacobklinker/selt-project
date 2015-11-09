a46509ea3d72e5c3aefbb50ce155b9d4d73770b8
tysonmassey <tyson-massey@uiowa.edu>
2015-11-09 17:55:58 +0000

    trying to get heroku working

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,12 +7,7 @@ class UserMailer < ApplicationMailer
     
     def league_invite(user,league_id)
         @user=user
-        #@url=url
         @league_id = league_id
-<<<<<<< HEAD
-        mail(to: @user, subject: "You're Invited!")
-=======
         mail(to: @user.email, subject: "You're Invited!")
->>>>>>> 1489ddc6b4d39ed60fa0b3feebfa015b63088217
     end
 end
