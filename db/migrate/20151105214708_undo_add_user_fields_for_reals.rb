b1f10f4ee6985b8109889663cc75d6f9ae92f7b4
Luke Klinker <lucas-klinker@uiowa.edu>
2015-11-12 15:25:23 +0000

    spelling mistake on migration

--- db/migrate/20151105214708_undo_add_user_fields_for_reals.rb
+++ db/migrate/20151105214708_undo_add_user_fields_for_reals.rb
@@ -1,6 +1,6 @@
 class UndoAddUserFieldsForReals < ActiveRecord::Migration
   def change
-    remove_column :users, :fist_name
+    remove_column :users, :first_name
     remove_column :users, :last_name
   end
 end
