e1c1471886645262c93a923166f81f79f8bd194b
Luke Klinker <lucas-klinker@uiowa.edu>
2015-11-16 16:54:16 +0000

    refactor homepage to model view controller architecture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,67 @@
 class UsersController < ApplicationController
+    before_filter :set_current_user
+  
+    def set_current_user
+      @user = current_user;
+    end
     
     def index
+        @leagues = [];
+        
+        if @user.league1_id != nil then
+            l = League.find(@user.league1_id);
+            u = User.find(l.commissioner_id);
+            league = {
+                :league => l,
+                :commissioner => u.first_name + " " + u.last_name,
+                :position => "1/12"
+            };
+            @leagues << league;
+        end
+        
+        if @user.league2_id != nil then
+            l = League.find(@user.league2_id);
+            u = User.find(l.commissioner_id);
+            league = {
+                :league => l,
+                :commissioner => u.first_name + " " + u.last_name,
+                :position => "1/12"
+            };
+            @leagues << league;
+        end
+        
+        if @user.league3_id != nil then
+            l = League.find(@user.league3_id)
+            u = User.find(l.commissioner_id);
+            league = {
+                :league => l,
+                :commissioner => u.first_name + " " + u.last_name,
+                :position => "1/12"
+            };
+            @leagues << league;
+        end
+        
+        if @user.league4_id != nil then
+            l = League.find(@user.league4_id)
+            u = User.find(l.commissioner_id);
+            league = {
+                :league => l,
+                :commissioner => u.first_name + " " + u.last_name,
+                :position => "1/12"
+            };
+            @leagues << league;
+        end
+        
+        if @user.league5_id != nil then
+            l = League.find(@user.league5_id);
+            u = User.find(l.commissioner_id);
+            league = {
+                :league => l,
+                :commissioner => u.first_name + " " + u.last_name,
+                :position => "1/12"
+            };
+            @leagues << league;
+        end
     end
     
     def sign_in
