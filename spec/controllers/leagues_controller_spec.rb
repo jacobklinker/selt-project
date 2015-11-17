daf6877d021965f349034a033964ccbef7e1defe
Luke Klinker <lucas-klinker@uiowa.edu>
2015-11-17 00:22:36 +0000

    uncomment some tests for the league controller

--- spec/controllers/leagues_controller_spec.rb
+++ spec/controllers/leagues_controller_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
-describe LeaguesController, type: :controller do
-  
+RSpec.describe LeaguesController, type: :controller do
+
+  # This should return the minimal set of attributes required to create a valid
+  # League. As you add validations to League, be sure to
+  # adjust the attributes here as well.
   let(:valid_attributes) {
     skip("Add a hash of attributes valid for your model")
   }
-  
+
   let(:invalid_attributes) {
     skip("Add a hash of attributes invalid for your model")
   }
-  
+
+  # This should return the minimal set of values that should be in the session
+  # in order to pass any filters (e.g. authentication) defined in
+  # LeaguesController. Be sure to keep this updated too.
   let(:valid_session) { {} }
-  
+
   describe "GET #index" do
     it "assigns all leagues as @leagues" do
       league = League.create! valid_attributes
