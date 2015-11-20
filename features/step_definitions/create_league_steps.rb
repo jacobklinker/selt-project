8f846421c4b7f70cf638babe0f109fd3a83f32dd
jacobklinker <jacob-klinker@uiowa.edu>
2015-11-20 17:07:50 +0000

    fix merge conflict

--- features/step_definitions/create_league_steps.rb
+++ features/step_definitions/create_league_steps.rb
@@ -4,13 +4,9 @@ When /^I create a league with "(.*?)" as a League Name and "(.*?)" as conference
   fill_in "email_list", :with => emails
   select conf_settings, :from => "league_conference_settings"
   select num_picks, :from => "league_number_picks_settings"
-<<<<<<< HEAD
   
   click_on 'Create League'
   puts page.body
-=======
-  click_on('work')
->>>>>>> 9a83e38d572875e4c17291286f5057a25644b229
 end
  
 When /^I am on the create new league page$/ do
