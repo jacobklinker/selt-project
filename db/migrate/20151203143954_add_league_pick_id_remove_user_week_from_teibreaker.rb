7b93187324cb0b158acec915366ee532ee6d3966
Luke Klinker <lucas-klinker@uiowa.edu>
2015-12-04 21:30:26 +0000

    try fixing migration

--- db/migrate/20151203143954_add_league_pick_id_remove_user_week_from_teibreaker.rb
+++ db/migrate/20151203143954_add_league_pick_id_remove_user_week_from_teibreaker.rb
@@ -1,6 +1,5 @@
 class AddLeaguePickIdRemoveUserWeekFromTeibreaker < ActiveRecord::Migration
   def change
-    remove_column :tiebreaker_picks, :user
     remove_column :tiebreaker_picks, :league
     remove_column :tiebreaker_picks, :week
     add_reference :tiebreaker_picks, :game, index: true, foreign_key: true
