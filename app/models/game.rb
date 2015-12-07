89ff8ecccb8a03aba418d21ccb511e323aebbe25
Luke Klinker <lucas-klinker@uiowa.edu>
2015-12-07 18:32:53 +0000

    check is finished instead of score

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,7 +4,7 @@ class Game < ActiveRecord::Base
     
     def self.home_team_cover
         Game.all.each do |game|
-            if (game.home_score != nil) then
+            if (game.is_finished) then
                 if((game.home_score+game.home_odds)==game.away_score)
                     game.homeTeamCover=1
                 elsif ((game.home_score+game.home_odds)>game.away_score)
